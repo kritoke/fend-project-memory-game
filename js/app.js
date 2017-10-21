e92690e220ba40930cfdec266347e05e34e31216
Matt Rhone <kritoke@gmail.com>
2017-10-21 12:08:39 -0500

    Add sortedDeck and shuffledDeck variables.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 /*
  * Create a list that holds all of your cards
  */
-
+const sortedDeck = ['diamond', 'diamond', 'paper-plane-o', 'paper-plane-o', 'anchor', 'anchor', 'bolt', 'bolt', 'cube', 'cube', 'leaf', 'leaf', 'bicycle', 'bicycle', 'bomb', 'bomb'];
+const shuffledDeck = shuffle(sortedDeck);
 
 /*
  * Display the cards on the page
@@ -12,7 +13,8 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
